7405645e6106c34a04374539f486c9cb3657bf47
Ghaadyy <ghadyyi@gmail.com>
2024-11-27 13:03:32 +0200

    Make the entire Restaurant Screen scrollable

--- app/src/main/java/com/example/deliveryapp/screens/RestaurantScreen.kt
+++ app/src/main/java/com/example/deliveryapp/screens/RestaurantScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Star
@@ -66,6 +68,7 @@ fun RestaurantScreen(onBack: () -> Unit) {
                 modifier = Modifier
                     .padding(paddingValues)
                     .padding(20.dp)
+                    .verticalScroll(rememberScrollState())
             ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
