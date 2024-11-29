e32e08554bf5384e160da98cd81e2f75cdaef062
A-Karam20 <antoinekaram1414@gmail.com>
2024-11-29 23:08:33 +0200

    Add enums for OrderRating and DriverRating

---
+++ app/src/main/java/com/example/deliveryapp/data/model/DriverRating.kt
@@ -0,0 +1,8 @@
+package com.example.deliveryapp.data.model
+
+enum class DriverRating(val ratingValue: Int, val label: String) {
+    LIKE(1, "Like"),
+    DISLIKE(-1, "Dislike"),
+    PENDING(0, "Pending"),
+    NOT_APPLICABLE(-2, "Not Applicable");
+}
