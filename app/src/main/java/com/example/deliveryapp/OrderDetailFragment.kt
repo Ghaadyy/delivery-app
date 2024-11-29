18536709859ea1e3917394ab2a7c482bfb86cdaa
Antoine Karam <antoine.karam1414@gmail.com>
2024-11-29 22:45:37 +0200

    Refactor: Extracted the first card of the OrderDetail fragment into its own fragment

--- app/src/main/java/com/example/deliveryapp/OrderDetailFragment.kt
+++ app/src/main/java/com/example/deliveryapp/OrderDetailFragment.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.deliveryapp.data.model.Order
 import com.example.deliveryapp.data.model.OrderDetail
-import com.example.deliveryapp.data.model.OrderStatus
 import com.example.deliveryapp.databinding.FragmentOrderDetailBinding
 import com.example.deliveryapp.ui.adapter.OrderDetailAdapter
 import com.example.deliveryapp.ui.viewModel.OrderViewModel
@@ -40,60 +39,16 @@ class OrderDetailFragment : Fragment() {
         binding.orderDetailRecyclerView.adapter = orderDetailAdapter
 
         orderViewModel._currentOrder.observe(viewLifecycleOwner) { order ->
-            bindFirstCard(order)
+            bindFirstCard()
             orderViewModel._currentOrderDetails.value?.let { bindSecondCard(it) }
             bindThirdCard(order)
         }
-
-        binding.trackOrderButton.setOnClickListener {
-            val orderTrackFragment = OrderTrackFragment()
-            val transaction = requireActivity().supportFragmentManager.beginTransaction()
-            transaction
-                .replace(R.id.fragment_container, orderTrackFragment)
-                .addToBackStack(null)
-                .commit()
-        }
     }
 
-    private fun bindFirstCard(order: Order){
-        val restaurant = order.restaurantId
-        val status = OrderStatus.fromId(order.status)
-        val driver = order.driverId
-        val message = if(status?.id == 6) {
-            "${status.label} on ${order.deliveredDate}"
-        }else{
-            "Placed at ${order.orderDate}"
-        }
-
-        binding.restaurantId.text = restaurant
-        binding.status.text = message
-        binding.driverName.text = driver
-        if(status?.id == 1 || status?.id == 2){
-            binding.trackOrderButton.visibility = View.VISIBLE
-            hideViews(
-                binding.driverLogo,
-                binding.driverName,
-                binding.dividerFirstCardSection2,
-                binding.reorderButton,
-                binding.dividerFirstCardSection3,
-                binding.orderRatingText,
-                binding.orderRatingStars,
-                binding.dividerFirstCardSection4,
-                binding.driverRatingText,
-                binding.driverRatingLogo
-            )
-        }else if(status?.id != 6){
-            binding.trackOrderButton.visibility = View.VISIBLE
-            hideViews(
-                binding.reorderButton,
-                binding.dividerFirstCardSection3,
-                binding.orderRatingText,
-                binding.orderRatingStars,
-                binding.dividerFirstCardSection4,
-                binding.driverRatingText,
-                binding.driverRatingLogo
-            )
-        }
+    private fun bindFirstCard() {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.orderCardFragment, OrderDetailOrderCardFragment())
+            .commit()
     }
 
     private fun bindSecondCard(orderDetails: List<OrderDetail>){
@@ -110,13 +65,6 @@ class OrderDetailFragment : Fragment() {
         binding.subTotal.text = subtotal
         binding.deliveryCharge.text = deliveryCharge
         binding.total.text = total
-    }
-
-    /**
-     * Utility function to hide multiple views
-     * */
-    private fun hideViews(vararg views: View) {
-        views.forEach { it.visibility = View.GONE }
     }
 
     override fun onDestroyView() {
