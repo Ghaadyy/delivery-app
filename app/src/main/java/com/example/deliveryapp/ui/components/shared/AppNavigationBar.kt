a6249a1e7a58c62498eb354d15ada230e2b02634
Ghaadyy <ghadyyi@gmail.com>
2024-12-01 23:23:44 +0200

    Added labels and Orders Tab

--- app/src/main/java/com/example/deliveryapp/ui/components/shared/AppNavigationBar.kt
+++ app/src/main/java/com/example/deliveryapp/ui/components/shared/AppNavigationBar.kt
@@ -7,9 +7,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -22,19 +24,25 @@ fun AppNavigationBar() {
                     Icons.Filled.Home,
                     "Home",
                 )
-            })
+            }, label = { Text("Home") })
             NavigationBarItem(selected = false, onClick = {}, icon = {
                 Icon(
                     Icons.Filled.Search,
                     "Browse",
                 )
-            })
+            }, label = { Text("Browse") })
+            NavigationBarItem(selected = false, onClick = {}, icon = {
+                Icon(
+                    Icons.Filled.ShoppingCart,
+                    "Orders",
+                )
+            }, label = { Text("Orders") })
             NavigationBarItem(selected = false, onClick = {}, icon = {
                 Icon(
                     Icons.Filled.Settings,
                     "Settings",
                 )
-            })
+            }, label = { Text("Settings") })
         }
     }
 }
