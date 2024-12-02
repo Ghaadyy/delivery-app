1c31637d7e4ad7e9c63a151d98197f0bfc4cb063
Ghaadyy <ghadyyi@gmail.com>
2024-12-02 17:05:42 +0200

    Added state for Option.kt

--- app/src/main/java/com/example/deliveryapp/ui/components/menu/option/Option.kt
+++ app/src/main/java/com/example/deliveryapp/ui/components/menu/option/Option.kt
@@ -4,12 +4,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 
 @Composable
 fun Option(name: String) {
+    var checked by remember { mutableStateOf(false) }
+
     Row(verticalAlignment = Alignment.CenterVertically) {
-        Checkbox(checked = true, onCheckedChange = {})
+        Checkbox(checked = checked, onCheckedChange = { isChecked -> checked = isChecked })
         Text("$name ${"+ LBP 360,000 (USD 4.0)"}")
     }
 }
