0cde3f67922c799336ee4d896e951d957298ae42
Ghaadyy <ghadyyi@gmail.com>
2024-12-02 18:57:55 +0200

    Added RestaurantsRepository

---
+++ app/src/main/java/com/example/deliveryapp/data/repository/RestaurantsRepository.kt
@@ -0,0 +1,10 @@
+package com.example.deliveryapp.data.repository
+
+import com.example.deliveryapp.data.model.menu.Menu
+import com.example.deliveryapp.data.model.Restaurant
+
+interface RestaurantsRepository {
+    suspend fun fetchRestaurants(): List<Restaurant>
+    suspend fun fetchRestaurant(id: String): Restaurant
+    suspend fun fetchMenu(restaurantId: String): Menu
+}
