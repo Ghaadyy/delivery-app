11beb3124d6bc9f6879d7e5c4eee73626140c824
Antoine Karam <antoine.karam1414@gmail.com>
2024-12-03 20:35:11 +0200

    Add RoomDB integration to fetch orders from remote, with fallback to local RoomDB on failure

--- app/src/main/java/com/example/deliveryapp/data/model/Order.kt
+++ app/src/main/java/com/example/deliveryapp/data/model/Order.kt
@@ -1,21 +1,31 @@
 package com.example.deliveryapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "orders",)
 data class Order(
-    val id: Int,
-    val customerId: Int,
-    val restaurantId: String,
-    val driver: Driver? = null,
-    var orderStatusId: Int,
-    val orderDate: String,
-    val deliveredDate: String,
-    val orderLocation: Location,
-    val subtotal: Double,
-    val discountedPrice: Double,
-    val deliveryCharge: Double,
-    val paymentMethod: String,
-    var orderRatingId: Int,
-    var driverRatingId: Int,
+    @PrimaryKey var id: Int,
+    @ColumnInfo(name = "customer_id") var customerId: Int,
+    @ColumnInfo(name = "restaurant_id") var restaurantId: String,
+    @Ignore val driver: Driver? = null,
+    @ColumnInfo(name = "order_status_id") var orderStatusId: Int,
+    @ColumnInfo(name = "order_date") var orderDate: String,
+    @ColumnInfo(name = "delivered_date") var deliveredDate: String,
+    @Ignore val orderLocation: Location,
+    @ColumnInfo(name = "subtotal") var subtotal: Double,
+    @ColumnInfo(name = "discounted_price") var discountedPrice: Double,
+    @ColumnInfo(name = "delivery_charge") var deliveryCharge: Double,
+    @ColumnInfo(name = "payment_method") var paymentMethod: String,
+    @ColumnInfo(name = "order_rating_id") var orderRatingId: Int,
+    @ColumnInfo(name = "driver_rating_id") var driverRatingId: Int,
 ){
+    constructor() : this(
+        0, -1, "", null, 0, "", "", Location(0.0, 0.0), 0.0, 0.0, 0.0, "", 0, 0
+    )
+
     val orderStatus: OrderStatus?
         get() = OrderStatus.fromId(orderStatusId)
 
---
+++ app/src/main/java/com/example/deliveryapp/data/repository/RemoteFirstOrderRepository.kt
@@ -0,0 +1,44 @@
+package com.example.deliveryapp.data.repository
+
+import android.content.Context
+import android.util.Log
+import com.example.deliveryapp.data.local.OrderDao
+import com.example.deliveryapp.data.local.OrdersDatabase
+import com.example.deliveryapp.data.model.Order
+import com.example.deliveryapp.data.model.OrderDetail
+import com.example.deliveryapp.data.service.OrderService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RemoteFirstOrderRepository(context: Context) : OrderRepository{
+    private val orderDao: OrderDao = OrdersDatabase.getInstance(context).orderDao()
+
+    override suspend fun fetchOrders(): List<Order> {
+        return try {
+            val ordersFromRemote = service.getOrders()
+            withContext(Dispatchers.IO) {
+                orderDao.insertOrders(ordersFromRemote)
+            }
+
+            ordersFromRemote
+        } catch (e: Exception) {
+            Log.d("RemoteFirstOrderRepository", e.message.toString())
+            withContext(Dispatchers.IO) {
+                orderDao.getOrders()
+            }
+        }
+    }
+
+    override suspend fun fetchOrderDetails(orderId: Int): List<OrderDetail> = service.getOrderDetails(orderId)
+
+    companion object {
+        private val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:5299/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        private val service: OrderService = retrofit.create(OrderService::class.java)
+    }
+}
