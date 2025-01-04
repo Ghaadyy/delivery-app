fcd2141e77b9f96a24b84f52bb67c2c7c3db9ea6
Ghaadyy <ghadyyi@gmail.com>
2025-01-04 23:31:11 +0200

    refactor: Removed unused Column in OrderRepository.kt

--- app/src/main/java/com/example/deliveryapp/ui/screens/orders/OrdersScreen.kt
+++ app/src/main/java/com/example/deliveryapp/ui/screens/orders/OrdersScreen.kt
@@ -1,14 +1,10 @@
 package com.example.deliveryapp.ui.screens.orders
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
@@ -26,27 +22,21 @@ fun OrdersScreen(
 
     Scaffold(
         content = { paddingValues ->
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier
-                    .padding(paddingValues)
-            ) {
-                AndroidView(
-                    modifier = Modifier.fillMaxSize(),
-                    factory = {
-                        context ->
-                        val frgId = FragmentContainerView(context).apply {
-                            id = R.id.fragment_container
-                        }
-                        val transaction = fragmentManager.beginTransaction()
-                        transaction.replace(R.id.fragment_container, OrderListFragment())
-                        transaction.addToBackStack(null)
-                        transaction.commit()
-                        frgId
-                    },
-                    update = {}
-                )
-            }
+            AndroidView(
+                modifier = Modifier.padding(paddingValues).fillMaxSize(),
+                factory = {
+                    context ->
+                    val frgId = FragmentContainerView(context).apply {
+                        id = R.id.fragment_container
+                    }
+                    val transaction = fragmentManager.beginTransaction()
+                    transaction.replace(R.id.fragment_container, OrderListFragment())
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                    frgId
+                },
+                update = {}
+            )
         },
         bottomBar = {
             AppNavigationBar(LocalNavController.current, screens)
